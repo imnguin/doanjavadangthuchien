aee31c6ab8571d0b02db822bebde1e86835b5a83
imnguin <lamnguyencoder@gmail.com>
2022-04-14 11:26:39 +0700

    cate repos

---
+++ jpacrud/src/main/java/com/doan/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.doan.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.doan.entities.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Long>{
+
+}
