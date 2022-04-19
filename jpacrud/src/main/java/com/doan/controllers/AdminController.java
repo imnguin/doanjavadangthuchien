9de3ac1748fdee7b5356a115ade96501d47610c0
imnguin <lamnguyencoder@gmail.com>
2022-04-19 11:20:02 +0700

    change admin interface
    Signed-off-by: imnguin <lamnguyencoder@gmail.com>

--- jpacrud/src/main/java/com/doan/controllers/AdminController.java
+++ jpacrud/src/main/java/com/doan/controllers/AdminController.java
@@ -3,6 +3,7 @@ package com.doan.controllers;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,23 +13,47 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.doan.entities.Category;
+import com.doan.entities.Comic;
 import com.doan.model.UploadedFile;
 import com.doan.services.CategoryService;
+import com.doan.services.ComicService;
 @Controller
 @RequestMapping("quan-tri")
 public class AdminController {
 
+	@Autowired
+    private ComicService comicService;
 	@Autowired CategoryService categoryService;
 	
 	@RequestMapping("/")
 	public String homepage(Model model) {
-	List<Category> danhmuc = categoryService.listAll();
-	model.addAttribute("dsdanhmuc", danhmuc);
-	return "admin/HomePage";
+	List<Comic> lstcommic = comicService.listAll();
+	model.addAttribute("lstcommic", lstcommic);
+	model.addAttribute("category", new Category());
+	return "admin/index";
 	}
+	
+	@RequestMapping("/danh-muc")
+	public String category(Model model) {
+		List<Category> danhmuc = categoryService.listAll();
+		model.addAttribute("dsdanhmuc", danhmuc);
+	return "admin/category";
+	}
+	
+	@RequestMapping("/them-danh-muc")
+	public String addCategory(Map<String, Object> model) {
+		Category cate = new Category();
+		model.put("category", cate);
+	return "admin/addcategory";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST) public String
+	 saveCategory(@ModelAttribute("category") Category category) {
+		categoryService.save(category); return "redirect:/quan-tri/danh-muc"; }
+	
 	@RequestMapping("/AddComic")
     public void saveFile(HttpServletRequest servletRequest, @ModelAttribute UploadedFile uploadedFile, BindingResult bindingResult, Model model) {
  
